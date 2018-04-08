c78cfbbfe8bc1ae8165c9a26c99ec1455d125b95
Amrit059 <35620061+Amrit059@users.noreply.github.com>
2018-04-09 00:48:44 +0530

    image upload by event by post request

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-upload',
@@ -6,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
-file(event){
-   console.log(event); 
+  SFile:File=null;
+  file(event){
+    this.SFile=<File>event.target.files[0]; 
 }
-  constructor() { }
-
+  constructor(private http:HttpClient) { }
+  upload(){ 
+    const  fd=new FormData()
+    fd.append('image',this.SFile,this.SFile.name)
+   this.http.post('https//localhost/4200/book',fd)
+   .subscribe(event=>
+   {console.log(event)
+   })}
   ngOnInit() {
   }
 
